75a207e6dd7a60fc57f7eac233a757ecfb73423c
Vit Dvorak <vidvorak0@gmail.com>
2023-02-21 22:33:42 +0100

    Copied code from previus GitLab repository

--- src/rt004/Program.cs
+++ src/rt004/Program.cs
@@ -1,27 +1,43 @@
-﻿using Util;
+﻿using rt004.Util;
+using Util;
 //using System.Numerics;
 
 namespace rt004;
 
 internal class Program
 {
-  static void Main(string[] args)
-  {
-    // Parameters.
-    // TODO: parse command-line arguments and/or your config file.
-    int wid = 600;
-    int hei = 450;
-    string fileName = "demo.pfm";
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> config = ArgumentParser.ParseParameters(args);
 
-    // HDR image.
-    FloatImage fi = new FloatImage(wid, hei, 3);
+        // HDR image.
+        FloatImage image = new FloatImage(int.Parse(config["width"]), int.Parse(config["height"]), 3);
 
-    // TODO: put anything interesting into the image.
-    // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
+        image = GenerateCosImage(image);
 
-    //fi.SaveHDR(fileName);   // Doesn't work well yet...
-    fi.SavePFM(fileName);
+        //fi.SaveHDR(fileName);   // Doesn't work well yet...
+        image.SavePFM(config["output"]);
 
-    Console.WriteLine("HDR image is finished.");
-  }
+        Console.WriteLine("HDR image is finished.");
+    }
+
+
+
+    /// <summary>
+    /// Writes to the image 2D consinus function
+    /// </summary>
+    /// <param name="image">image to write to</param>
+    private static FloatImage GenerateCosImage(FloatImage image)
+    {
+        for (int y = 0; y < image.Height; y++)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                float pixelValue = (float)((Math.Cos(x / 10.0f) * Math.Cos(y / 10.0f) + 1) * image.Width);
+                float[] pixel = new float[3] { pixelValue, pixelValue, pixelValue };
+                image.PutPixel(x, y, pixel);
+            }
+        }
+        return image;
+    }
 }
---
+++ src/rt004/Util/ArgumentParser.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace rt004.Util
+{
+    internal static class ArgumentParser
+    {
+        /// <summary>
+        /// Parses Comand line parameters and if parameter is not specified then it useis configFile to define
+        /// </summary>
+        /// <param name="args">command line arguments</param>
+        /// <param name="defaultConfigFile">default Path to configuration file</param>
+        /// <returns>Inicialized config dictionary containing only specified Keys</returns>
+        /// <exception cref="ArgumentException">Thrown when argument is not found in default </exception>
+        public static Dictionary<string, string> ParseParameters(string[] args, string defaultConfigFile = "config.txt")
+        {
+            var config = new Dictionary<string, string>()
+            {
+                // Default values of all supported parameters
+                ["config"] = defaultConfigFile,
+                ["width"] = "600",
+                ["height"] = "450",
+                ["output"] = "output.pfm"
+            };
+
+            var argumentLine = String.Join('\n', args);
+            byte[] argumentLineBytes = Encoding.Unicode.GetBytes(argumentLine);
+
+            // parse command line arguments to potencialy change config File path
+            using (var reader = new StreamReader(new MemoryStream(argumentLineBytes)))
+            {
+                config = Parse(reader, config);
+            }
+
+            // parsing config file arguments
+            using (StreamReader reader = File.OpenText(config["config"]))
+            {
+                config = Parse(reader, config);
+            }
+
+            // override config file parameters with command line arguments
+            using (StreamReader reader = new StreamReader(new MemoryStream(argumentLineBytes)))
+            {
+                config = Parse(reader, config);
+            }
+
+            return config;
+        }
+
+        /// <summary>
+        /// Parses parameters from streamReader
+        /// </summary>
+        /// <param name="inputStream">Stream to parse</param>
+        /// <param name="config">configuration dictionary to save the data</param>
+        /// <returns>Returns input config dictionary with filled config parameters.</returns>
+        /// <exception cref="ArgumentException">thrown when there is any problem with format of arguments</exception>
+        private static Dictionary<string, string> Parse(StreamReader inputStream, Dictionary<string, string> config)
+        {
+            uint lineNumber = 0;
+            while (!inputStream.EndOfStream)
+            {
+                lineNumber++;
+                string line = inputStream.ReadLine().Trim();
+
+                if (line.Length != 0)//skip empty lines
+                {
+                    string[] paramValues = line.Split('=');
+                    for (int i = 0; i < paramValues.Length; i++)
+                    {
+                        paramValues[i] = paramValues[i].Trim();
+                        paramValues[i] = paramValues[i].Trim('"');
+                    }
+
+
+                    if (paramValues.Length != 2)
+                    {
+                        throw new ArgumentException($"Argument format problem at line {lineNumber}", line);
+                    }
+                    else if (config.ContainsKey(paramValues[0]))
+                    {
+                        config[paramValues[0].ToLower()] = paramValues[1];
+                    }
+                    else
+                    {
+                        throw new ArgumentException("unknown parameter name", paramValues[0]);
+                    }
+                }
+            }
+            return config;
+        }
+    }
+}
