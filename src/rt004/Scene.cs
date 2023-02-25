8e10ed3afae7cf006c66bd77c7bf634638289a42
Vit Dvorak <vidvorak0@gmail.com>
2023-02-25 12:06:29 +0100

    Scene Inplemented

--- src/rt004/Scene.cs
+++ src/rt004/Scene.cs
@@ -1,26 +1,49 @@
 ﻿using rt004.Bodies;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using Util;
 
 namespace rt004
 {
     internal class Scene
     {
-        public void AddCamera()
+        private HashSet<Camera> cameras = new HashSet<Camera>();
+        private HashSet<Body> bodies = new HashSet<Body>();
+
+        public void AddCamera(Camera camera)
         {
-            throw new NotImplementedException();
+            cameras.Add(camera);
         }
 
-        public Camera[] GetCameras() { throw new NotImplementedException(); }
-        Camera RemoveCamera()
-        {
-            throw new NotImplementedException();
+        public Camera[] GetCameras() {
+            return cameras.ToArray();
         }
 
-        public void AddBody (Body body) { throw new NotImplementedException();}
-        public void RemoveBody (Body body) { throw new NotImplementedException(); }
-        public FloatImage RenderScene() { throw new NotImplementedException(); }
-        public FloatImage RenderSceneWithAllCameras() { throw new NotImplementedException();}
+        public void RemoveCamera(Camera camera)
+        {
+            cameras.Remove(camera);
+        }
 
+        public void AddBody (Body body) => bodies.Add(body);
+
+        public void RemoveBody (Body body) => bodies.Remove(body);
+
+        public Body[] GetBodies() => bodies.ToArray();
+
+        public FloatImage RenderScene() {
+            Camera camera = GetCameras()[0];
+
+            return camera.RenderImage(GetBodies());
+        }
+
+        public FloatImage[] RenderSceneWithAllCameras() {
+            List<FloatImage> images = new List<FloatImage>();
+            Body[] bodies = GetBodies();
+            foreach (Camera camera in cameras)
+            {
+                images.Add(camera.RenderImage(bodies));
+            }
+            return images.ToArray();
+        }
     }
 }
