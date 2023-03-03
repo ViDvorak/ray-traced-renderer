3d10630dcc30767148f5ded6ce55c44e2d125a34
Vit Dvorak <vidvorak0@gmail.com>
2023-03-03 23:34:50 +0100

    implemented Camera mathods and added Properties

--- src/rt004/Camera.cs
+++ src/rt004/Camera.cs
@@ -4,41 +4,96 @@ using Util;
 
 namespace rt004
 {
-    internal class Camera
+    internal class Camera : SceneObject
     {
-        public Vector3 position;
-        private Vector3 rotation;
-        public Vector3 Rotation
-        {
-            get { return rotation; }
+        public Color4 beckgroundColor;
+
+        private const float maxFoV = float.Pi - float.Pi / 180; // 179° in radians
+        private float fov;
+        
+
+        public float FoV {
+            get
+            {
+                return fov;
+            }
             set
             {
-                Vector3 tmp = value / MathF.PI;
-                tmp.X = MathF.Floor(tmp.X);
-                tmp.Y = MathF.Floor(tmp.Y);
-                tmp.Z = MathF.Floor(tmp.Z);
-                rotation = value - tmp * MathF.PI;
+                if (value >= maxFoV || value < 0 ) { throw new ArgumentException($"FoV not in range 0 - PI value was {value}"); }
+                fov = value;
             }
         }
 
-        public Color4 beckgroundColor;
-        public float FoV;
-
         private uint width, height;
 
-        public uint Width { get { return width; } }
-        public uint Height { get { return height; } }
+        public uint Width {
+            get { return width; }
+            set { width = value; }
+        }
+        public uint Height {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private float maxRenderingDistanceSquered = MathF.Sqrt( float.MaxValue);
+        public float MaxRenderingDistance
+        {
+            get
+            {
+                return MathF.Sqrt(maxRenderingDistanceSquered);
+            }
+            set
+            {
+                maxRenderingDistanceSquered = value * value;
+            }
+        }
+
+        public Camera(Vector3 position, Vector3 rotation, Color4 bacgroudColor, float fov, uint width, uint height) : base(position, rotation)
+        {
+            beckgroundColor = bacgroudColor;
+            FoV = fov;
+            this.width = width;
+            this.height = height;
+        }
 
         public void SetResolution(uint width, uint height)
         {
-            this.width  = width;
-            this.height = height;
+            Width = width;
+            Height = height;
         }
 
         public FloatImage RenderImage(Body[] RenderedBodies)
         {
-            throw new NotImplementedException();
-        }
+            FloatImage image = new FloatImage((int)Width, (int)Height, 3);
 
+            float distanceOfIntersection = maxRenderingDistanceSquered;
+
+            for (int x = 0; x < Width; ++x)
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    Color4 color = beckgroundColor;
+
+                    foreach (var body in RenderedBodies)
+                    {
+                        var pixelDensity = Width / (2 * Math.Tan(fov / 2));
+                        Vector3 pixelVectorInObjectSpace = new Vector3(x - width / 2, y - height / 2, 1); // position == rayVector in object space
+                        Vector3 pixelRayDirection = ConvertFromObjectToWorldSpace(pixelVectorInObjectSpace);
+
+                        if (body.TryGetRayIntersection(Position, pixelRayDirection, out Vector3 intersection))
+                        {
+                            var cameraToIntersection = intersection - Position;
+                            if (cameraToIntersection.LengthSquared <= maxRenderingDistanceSquered)
+                            {
+                                distanceOfIntersection = cameraToIntersection.Length;
+                                color = body.color;
+                            }
+                        }
+                    }
+                    image.PutPixel(x, y, new float[] { color.R, color.G, color.B });
+                }
+            }
+            return image;
+        }
     }
 }
