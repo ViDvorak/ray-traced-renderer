83f34f3d4f6fab4d716ffbe319125981a320845c
Vit Dvorak <vidvorak0@gmail.com>
2023-03-21 15:38:13 +0100

    Camera looks in direxion of local x axis

---
+++ src/rt004/SceneObjects/Camera.cs
@@ -0,0 +1,132 @@
+﻿using OpenTK.Mathematics;
+using rt004.SceneObjects;
+using rt004.Util;
+using System.Xml.Serialization;
+using Util;
+
+namespace rt004.SceneObjects
+{
+    public class Camera : SceneObject
+    {
+        public Color4 backgroundColor = RendererSettings.defaultBacgroundColor;
+
+        private const float maxFoV = float.Pi - float.Pi / 180; // 179° in radians
+        private float fov;
+
+
+        public float FoV
+        {
+            get
+            {
+                return fov;
+            }
+            set
+            {
+                if (value >= maxFoV || value < 0) { throw new ArgumentException($"FoV not in range (0, PI) value was {value}"); }
+                fov = value;
+            }
+        }
+
+        public uint width, height;
+
+        private float maxRenderingDistanceSquered = MathF.Sqrt(float.MaxValue);
+        public float MaxRenderingDistance
+        {
+            get
+            {
+                return MathF.Sqrt(maxRenderingDistanceSquered);
+            }
+            set
+            {
+                maxRenderingDistanceSquered = value * value;
+            }
+        }
+
+        public Camera() : base(Vector3.Zero, Vector3.Zero)
+        {
+            this.backgroundColor = RendererSettings.defaultBacgroundColor;
+            fov = float.Pi / 3;
+            this.width = 1280;
+            this.height = 720;
+        }
+
+        public Camera(Vector3 position, Vector3 rotation, Color4 bacgroudColor, float fov, uint width, uint height) : base(position, rotation)
+        {
+            backgroundColor = bacgroudColor;
+            FoV = fov;
+            this.width = width;
+            this.height = height;
+        }
+
+        public void SetResolution(uint width, uint height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public FloatImage RenderImage(Solid[] RenderedBodies)
+        {
+            FloatImage image = new FloatImage((int)width, (int)height, 3);
+
+            float distanceOfIntersectionSquared = maxRenderingDistanceSquered;
+
+            for (int z = 0; z < width; ++z)
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    Color4 color = backgroundColor;
+
+                    //z = (int)width / 2;
+                    //y = (int)height / 2;
+
+                    foreach (var body in RenderedBodies)
+                    {
+                        float pixelDensity = width / (2 * MathF.Tan(fov / 2));
+                        Vector3 pixelVectorInObjectSpace = new Vector3(1, (y - height / 2) / pixelDensity, -(z - width / 2) / pixelDensity); // position == rayVector in object space
+                        Vector3 pixelRayDirection = ConvertFromObjectToWorldSpace(pixelVectorInObjectSpace, false);
+
+                        Line ray = new Line(Position, pixelRayDirection);
+
+                        if (body.TryGetRayIntersection(ray, out Vector3 intersection))
+                        {
+                            var cameraToIntersection = intersection - Position;
+                            if (cameraToIntersection.LengthSquared <= distanceOfIntersectionSquared)
+                            {
+                                distanceOfIntersectionSquared = cameraToIntersection.LengthSquared;
+                                color = body.color;
+                            }
+                        }
+                    }
+                    image.PutPixel(z, y, new float[] { color.R, color.G, color.B });
+                }
+            }
+            return image;
+        }
+    }
+}
+
+namespace rt004.SceneObjects.Loading
+{
+    public class CameraLoader : SceneObjectLoader
+    {
+        public Color4 backgroundColor;
+        public float fov;
+
+        public uint width, height;
+
+        public CameraLoader() { }
+
+        public CameraLoader(Vector3 position, Vector3 rotation, Color4 backgroundColor, float fov, uint width, uint height) : base(position, rotation)
+        {
+            this.backgroundColor = backgroundColor;
+            this.fov = fov;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override SceneObject CreateInstance()
+        {
+            return new Camera(position, rotation, backgroundColor, fov, width, height);
+        }
+    }
+}
---
+++ src/rt004/SceneObjects/SceneObject.cs
@@ -0,0 +1,107 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace rt004.SceneObjects
+{
+    public abstract class SceneObject
+    {
+        private Vector3 position;
+        private Vector3 rotation;
+
+        private Matrix4 transformMatrix;
+
+        private Scene parentScene;
+
+        public Vector3 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+                transformMatrix = CreateTransformMatrix();
+            }
+        }
+
+        public Vector3 Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                // modulo 2Pi
+                Vector3 tmp = value / (2 * MathF.PI);
+                tmp.X = MathF.Floor(tmp.X);
+                tmp.Y = MathF.Floor(tmp.Y);
+                tmp.Z = MathF.Floor(tmp.Z);
+                rotation = value - 2 * MathF.PI * tmp;
+
+                transformMatrix = CreateTransformMatrix();
+            }
+        }
+
+        public Scene ParentScene
+        {
+            get { return parentScene; }
+            set {
+                if (value == null) { throw new ArgumentNullException(); }
+                parentScene = value;
+            }
+        }
+
+        public SceneObject(Vector3 position, Vector3 rotation)
+        {
+            this.position = position;
+            Rotation = rotation;
+
+            transformMatrix = CreateTransformMatrix();
+        }
+
+        public Vector3 ConvertFromObjectToWorldSpace(Vector3 vectorInObjectSpace, bool isPosition)
+        {
+            if (isPosition)
+                return Vector3.Add(position, vectorInObjectSpace);
+            else
+                return Vector3.Transform(vectorInObjectSpace, new Quaternion(Rotation));
+        }
+
+        private Matrix4 CreateTransformMatrix()
+        {
+            var rotationMatrix = Matrix4.CreateFromQuaternion(new Quaternion(rotation));
+            var translationMatrix = Matrix4.CreateTranslation(position);
+            return rotationMatrix * translationMatrix;
+        }
+    }
+
+}
+
+namespace rt004.SceneObjects.Loading {
+    [XmlInclude(typeof(CameraLoader)), XmlInclude(typeof(SolidLoader)), XmlInclude(typeof(LightSourceLoader))]
+    public abstract class SceneObjectLoader
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+
+        public SceneObjectLoader() { }
+
+        public SceneObjectLoader(Vector3 position, Vector3 rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
+
+
+
+        /// <summary>
+        /// Cteates Instance of corresponding SceneObject type
+        /// </summary>
+        /// <returns>instanciated object</returns>
+        abstract public SceneObject CreateInstance();
+    }
+}
