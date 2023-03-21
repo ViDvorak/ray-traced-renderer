e4bd73a464d8ca34c2fdbed4694ce22a92d8a21d
Vit Dvorak <vidvorak0@gmail.com>
2023-03-21 15:41:38 +0100

    added extenstion functions for imprecise equality to float and OpenTk.Vector3

---
+++ src/rt004/Util/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK.Mathematics;
+using rt004.Util;
+
+namespace rt004
+{
+    public static class Extensions
+    {
+        public static bool isFloatEquals(this float value, float other)
+        {
+            return value + RendererSettings.floatPrecision > other && value - RendererSettings.floatPrecision < other;
+        }
+
+        public static bool isVectorEquals(this Vector3 vector, Vector3 other)
+        {
+            return  vector[0].isFloatEquals(other[0]) &&
+                    vector[1].isFloatEquals(other[1]) &&
+                    vector[2].isFloatEquals(other[2]);
+        }
+    }
+}
