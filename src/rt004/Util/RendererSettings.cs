40902a10e5b59c22a042d285718e6ccc93a207f5
Vit Dvorak <vidvorak0@gmail.com>
2023-03-21 15:45:10 +0100

    Added static class for setting global renderer settings

---
+++ src/rt004/Util/RendererSettings.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rt004.Util
+{
+    internal static class RendererSettings
+    {
+        public static float floatPrecision = 0.001f;
+
+        public static Color4 defaultBacgroundColor = Color4.Gray;
+        public static Color4 defaultSolidColor = Color4.Lime;
+    }
+}
