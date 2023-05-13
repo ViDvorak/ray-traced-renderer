c557adce812f7e06b23f39d9a8cf88757359d07c
Vit Dvorak <vidvorak0@gmail.com>
2023-05-13 18:44:20 +0200

    Input Xml file for Scene definition now supports variables

--- src/rt004/Util/ArgumentParser.cs
+++ src/rt004/Util/ArgumentParser.cs
@@ -6,132 +6,200 @@ using System.Xml.Serialization;
 using rt004.SceneObjects.Loading;
 using rt004.Materials.Loading;
 using rt004.Materials;
+using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
 
 namespace rt004.Util
 {
-    public static class ArgumentParser
-    {
-        /// <summary>
-        /// Parses Comand line parameters. If parameter is not specified, then it uses XML configFile to define it.
-        /// </summary>
-        /// <param name="args">command line arguments</param>
-        /// <param name="configFile">default Path to XML configuration file</param>
-        /// <returns>loaded data</returns>
-        /// <exception cref="ArgumentException">Thrown when argument is not found in default </exception>
-        private static DataLoader ParseParameters(string[] args, string configFile)
-        {
+	public static class ArgumentParser
+	{
+		/// <summary>
+		/// Parses Comand line parameters. If parameter is not specified, then it uses XML configFile to define it.
+		/// </summary>
+		/// <param name="args">command line arguments</param>
+		/// <param name="configFile">default Path to XML configuration file</param>
+		/// <returns>loaded data</returns>
+		/// <exception cref="ArgumentException">Thrown when argument is not found in default </exception>
+		private static DataLoader ParseParameters(string[] args, string configFile)
+		{
 
-            var config = new Dictionary<string, string>()
-            {
-                // Default values of all supported parameters
-                ["config"] = configFile,
-                ["output"] = "output.pfm",
-            };
+			var config = new Dictionary<string, string>()
+			{
+				// Default values of all supported parameters
+				["config"] = configFile,
+				["output"] = "output.pfm",
+			};
 
-            var argumentLine = String.Join('\n', args);
-            byte[] argumentLineBytes = Encoding.Unicode.GetBytes(argumentLine);
+			var argumentLine = String.Join('\n', args);
+			byte[] argumentLineBytes = Encoding.Unicode.GetBytes(argumentLine);
 
-            // parse command line arguments to potencialy change config File path
-            using (var reader = new StreamReader(new MemoryStream(argumentLineBytes)))
-            {
-                config = Parse(reader, config);
-            }
+			// parse command line arguments to potencialy change config File path
+			using (var reader = new StreamReader(new MemoryStream(argumentLineBytes)))
+			{
+				config = Parse(reader, config);
+			}
 
-            var serializer = new XmlSerializer(typeof(DataLoader));
+			var serializer = new XmlSerializer(typeof(DataLoader));
 
-            DataLoader data = new DataLoader();
-            
-            /*
-            // tests
-            data.output = "output.pfm";
-            
-            SceneLoader scene = new SceneLoader();
-            TextureLoader whiteUniform = new UniformTextureLoader(Color4.White);
+			DataLoader data = new DataLoader();
+			
+			/*
+			// tests
+			data.output = "output.pfm";
+			
+			SceneLoader scene = new SceneLoader();
+			TextureLoader whiteUniform = new UniformTextureLoader(Color4.White);
 
-            
-            var material = new MaterialLoader(Color4.Gold, 0.5f, 0.5f, 1f);
+			
+			var material = new MaterialLoader(Color4.Gold, 0.5f, 0.5f, 1f);
 
-            scene.solidLoaders.Add(new SphereLoader(new Point3D(1, 2, 3), new Vector3(3, 2, 1), material, 1));
-            scene.solidLoaders.Add(new PlaneLoader(new Point3D(0, 0, 0), new Vector3(0, 0, 0), material));
+			scene.solidLoaders.Add(new SphereLoader(new Point3D(1, 2, 3), new Vector3(3, 2, 1), material, 1));
+			scene.solidLoaders.Add(new PlaneLoader(new Point3D(0, 0, 0), new Vector3(0, 0, 0), material));
 
-            scene.lightLoaders.Add(new PointLightLoader(new Point3D(5,5,5), Vector3.Zero, Color4.Azure, 10, 0.5f, 0.5f));
-            scene.cameraLoaders.Add( new CameraLoader(Point3D.Zero, Vector3.Zero, Color4.DimGray, MathF.PI / 2, 780, 600));
-            data.sceneLoader = scene;
+			scene.lightLoaders.Add(new PointLightLoader(new Point3D(5,5,5), Vector3.Zero, Color4.Azure, 10, 0.5f, 0.5f));
+			scene.cameraLoaders.Add( new CameraLoader(Point3D.Zero, Vector3.Zero, Color4.DimGray, MathF.PI / 2, 780, 600));
+			data.sceneLoader = scene;
 
-            serializer.Serialize(Console.Out, data); // Problem not Serialized, result is {}
-            */
-
-
-            // parsing XML config file
-            using (Stream stream = File.Open(config["config"], FileMode.Open))
-            {
-                data = (DataLoader)serializer.Deserialize(stream);
-            }
-
-            data.output = config["output"];
-
-            return data;
-        }
-
-        /// <summary>
-        /// Parses parameters from streamReader.
-        /// </summary>
-        /// <param name="inputStream">Stream to parse</param>
-        /// <param name="config">configuration dictionary to save the data</param>
-        /// <returns>Returns input config dictionary with filled config parameters.</returns>
-        /// <exception cref="ArgumentException">thrown when there is any problem with format of arguments</exception>
-        private static Dictionary<string, string> Parse(StreamReader inputStream, Dictionary<string, string> config)
-        {
-            uint lineNumber = 0;
-            while (!inputStream.EndOfStream)
-            {
-                lineNumber++;
-                string line = inputStream.ReadLine();
-
-                if (line.Length != 0)//skip empty lines
-                {
-                    line = line.Trim();
-                    string[] paramValues = line.Split('=', 2, StringSplitOptions.TrimEntries);
-                    for (int i = 0; i < paramValues.Length; i++)
-                    {
-                        paramValues[i] = paramValues[i].Trim('"');
-                    }
+			serializer.Serialize(Console.Out, data);
+			*/
 
 
-                    if (paramValues.Length != 2)
-                    {
-                        throw new ArgumentException($"Argument format problem at line {lineNumber}", line);
-                    }
-                    else if (config.ContainsKey(paramValues[0]))
-                    {
-                        config[paramValues[0].ToLower()] = paramValues[1];
-                    }
-                    else
-                    {
-                        throw new ArgumentException($"unknown parameter name at line {lineNumber}", paramValues[0]);
-                    }
-                }
-            }
-            return config;
-        }
+			// parsing XML config file
+			using (Stream stream = File.Open(config["config"], FileMode.Open))
+			{
+                (string inputWithoutVariables, Dictionary<string,string> variables) = InitializeVariables(new StreamReader(stream));
+				var xmlData = replaceVariables(inputWithoutVariables, variables);
 
-        /// <summary>
-        /// Parses config file to scene and output path.
-        /// </summary>
-        /// <param name="args">Command line parameters</param>
-        /// <param name="configFile">Path to config file</param>
-        /// <returns>Returns tuple (initialized Scean, output path where to save rendered image)</returns>
-        public static (Scene, string) ParseScene( string[] args, string configFile = "config.xml")
-        {
-            DataLoader loadedData = ParseParameters(args, configFile);
+				data = (DataLoader)serializer.Deserialize(new StringReader(xmlData));
+			}
 
-            return (loadedData.sceneLoader.CreateInstance(), loadedData.output);
-        }
+			data.output = config["output"];
 
-        public struct DataLoader
-        {
-            public string output;
-            public SceneLoader sceneLoader;
-        }
-    }
+			return data;
+		}
+
+		/// <summary>
+		/// Parses parameters from streamReader.
+		/// </summary>
+		/// <param name="inputStream">Stream to parse</param>
+		/// <param name="config">configuration dictionary to save the data</param>
+		/// <returns>Returns input config dictionary with filled config parameters.</returns>
+		/// <exception cref="ArgumentException">thrown when there is any problem with format of arguments</exception>
+		private static Dictionary<string, string> Parse(StreamReader inputStream, Dictionary<string, string> config)
+		{
+			uint lineNumber = 0;
+			while (!inputStream.EndOfStream)
+			{
+				lineNumber++;
+				string line = inputStream.ReadLine();
+
+				if (line.Length != 0)//skip empty lines
+				{
+					line = line.Trim();
+					string[] paramValues = line.Split('=', 2, StringSplitOptions.TrimEntries);
+					for (int i = 0; i < paramValues.Length; i++)
+					{
+						paramValues[i] = paramValues[i].Trim('"');
+					}
+
+
+					if (paramValues.Length != 2)
+					{
+						throw new ArgumentException($"Argument format problem at line {lineNumber}", line);
+					}
+					else if (config.ContainsKey(paramValues[0]))
+					{
+						config[paramValues[0].ToLower()] = paramValues[1];
+					}
+					else
+					{
+						throw new ArgumentException($"unknown parameter name at line {lineNumber}", paramValues[0]);
+					}
+				}
+			}
+			return config;
+		}
+
+
+
+
+
+		private static (string, Dictionary<string,string>) InitializeVariables(TextReader reader)
+		{
+            Dictionary<string, string> variables = new Dictionary<string, string>();
+
+            string? input = reader.ReadToEnd();
+
+			if (input == null)
+				throw new ArgumentNullException("io input has been loaded");
+
+
+            int startIndex = input.IndexOf("<variables>") + "<variables>\n".Length + 1;
+            int endIndex = input.IndexOf("</variables>");
+
+            string variableDefinitions = input.Substring(startIndex, endIndex - startIndex);
+
+            variables = ParseVariables(variableDefinitions);
+			return (input, variables);
+		}
+
+
+		private static Dictionary<string, string> ParseVariables(string variableDefinitions)
+		{
+			Dictionary<string, string> result = new Dictionary<string, string>();
+
+			string xmlTagPattern = @"\s*\b(\w+)\b.+\1\b";
+
+			foreach (Match match in Regex.Matches(variableDefinitions, xmlTagPattern, RegexOptions.Singleline))
+			{
+				var variableName = match.Groups[1].Value;
+				string variableContentPattern = $"{variableName}>" + @"\s*(.*)\s*" + $"</{variableName}";
+
+				//Match variable content
+				Match variableContentMatch = Regex.Match(match.Groups[0].Value, variableContentPattern, RegexOptions.Singleline);
+				if (variableContentMatch.Success)
+				{
+					result.Add(variableName, variableContentMatch.Groups[1].Value);
+				}
+				else
+				{
+					throw new ArgumentException("variable content not found");
+				}
+			}
+			return result;
+		}
+
+
+
+		public static string replaceVariables(string input, Dictionary<string, string> variableDefinitions)
+		{
+			foreach ( (string name, string content) in variableDefinitions)
+			{
+				input = Regex.Replace(input, $"<{name}/>", content);
+			}
+			return input;
+		}
+
+
+
+
+		/// <summary>
+		/// Parses config file to scene and output path.
+		/// </summary>
+		/// <param name="args">Command line parameters</param>
+		/// <param name="configFile">Path to config file</param>
+		/// <returns>Returns tuple (initialized Scean, output path where to save rendered image)</returns>
+		public static (Scene, string) ParseScene( string[] args, string configFile = "config.xml")
+		{
+			DataLoader loadedData = ParseParameters(args, configFile);
+
+			return (loadedData.sceneLoader.CreateInstance(), loadedData.output);
+		}
+
+		public struct DataLoader
+		{
+			public string output;
+			public SceneLoader sceneLoader;
+		}
+	}
 }
---
+++ src/rt004/Util/VariableTextReader.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace rt004.Util
+{
+    internal class VariableTextReader : TextReader
+    {
+        Dictionary<string, string> variables = new Dictionary<string, string>();
+
+        string initialString = "";
+        int currentIndexInInitialString = 0;
+
+
+        public VariableTextReader(Stream stream)
+        {
+            InitializeVariables(new StreamReader(stream));
+            int start = initialString.IndexOf("<variables>");
+            int end = initialString.IndexOf("</variables>") + "<variables>\n".Length + 1;
+
+            if (start > 0 && end > 0)
+            {
+                initialString = initialString.Remove(start, end - start);
+            }
+        }
+
+
+        public override int Read()
+        {
+            if (currentIndexInInitialString + 1 < initialString.Length)
+                return initialString[++currentIndexInInitialString];
+            return base.Read();
+        }
+
+        public override int Peek()
+        {
+            if(currentIndexInInitialString < initialString.Length)
+                return initialString[currentIndexInInitialString];
+            return base.Peek();
+        }
+
+        private void InitializeVariables(TextReader reader)
+        {
+            StringBuilder initialString = new StringBuilder();
+            string? str = reader.ReadLine();
+            bool hasEndOfVariable = false;
+
+            while (str != null)
+            {
+                initialString.AppendLine(str);
+                if (hasEndOfVariable)
+                {
+                    break;
+                }
+                
+                str = reader.ReadLine();
+                hasEndOfVariable = str.Contains("</variables>");
+            }
+            this.initialString = initialString.ToString();
+            variables = ParseVariables(this.initialString);
+        }
+
+
+        private Dictionary<string, string> ParseVariables(string parseFrom)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            string xmlTagPattern = @"\s*\b(\w+)\b.+\1\b";
+
+            int startIndex = parseFrom.IndexOf("<variables>") + "<variables>\n".Length + 1;
+            int endIndex = parseFrom.IndexOf("</variables>");
+
+            string variableDefinitions = parseFrom.Substring(startIndex, endIndex - startIndex);
+
+            foreach (Match match in Regex.Matches(variableDefinitions, xmlTagPattern, RegexOptions.Singleline))
+            {
+                var variableName = match.Groups[1].Value;
+                string variableContentPattern = $"{variableName}>" + @"\s*(.*)\s*" + $"</{variableName}";
+                
+                //Match variable content
+                Match variableContentMatch = Regex.Match(match.Groups[0].Value, variableContentPattern, RegexOptions.Singleline);
+                if (variableContentMatch.Success)
+                {
+                    result.Add(variableName, variableContentMatch.Groups[1].Value);
+                }
+                else
+                {
+                    throw new ArgumentException("variable content not found");
+                }
+            }
+            return result;
+        }
+    }
+}
